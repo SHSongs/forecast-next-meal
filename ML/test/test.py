94feac9efcbcc3c1590efe79af7bbe88f56f446c
SONG <simpledeveloper.songs@gmail.com>
2021-09-30 11:00:37 +0900

    using show tensor img func

--- ML/test/test.py
+++ ML/test/test.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
+from util import show_tensor_img
 from model import UNet
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -26,24 +27,12 @@ class TestModel(unittest.TestCase):
         img = m(img)
 
         print(img.shape)
-        self.assertTrue((1, 3, 160, 200) == img.size())
 
         out = model(img)
-        self.assertTrue((1, 3, 160, 200) == out.size())
 
-        fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
-        fn_denorm = lambda x, mean, std: (x * std) + mean
-
-        img = fn_tonumpy(img)
-
-        out = fn_tonumpy(fn_denorm(out, mean=0.5, std=0.5))
-        out = np.clip(out, a_min=0, a_max=1)
-
-        print(out.shape)
-
-        plt.imsave('input.png', img[0])
-        plt.imsave('output.png', out[0])
-
+        show_tensor_img(img)
+        show_tensor_img(out)
+        
 
 if __name__ == "__main__":
     unittest.main()
