e7d090735ecaf6c0bdc31f7da2a87f4ce0ed3763
SONG <simpledeveloper.songs@gmail.com>
2021-09-30 17:18:09 +0900

    test_dataset

--- ML/test/test.py
+++ ML/test/test.py
@@ -35,12 +35,34 @@ class TestModel(unittest.TestCase):
 
 
 from torchvision import transforms
-from dataset import Dataset, ToTensor
+from dataset import *
 
 
 class TestData(unittest.TestCase):
     def test_dataset(self):
-        dataset = Dataset(input_dir="data/input", label_dir="data/label")
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        model = UNet(nch=3, nker=64, norm="bnorm", learning_type="plain")
+
+        transform_train = transforms.Compose([Normalization(), ToTensor(), Padding()])
+
+        dataset_train = Dataset(label_dir="data/label", input_dir="data/input", transform=transform_train)
+
+        loader_train = DataLoader(dataset_train, batch_size=1, shuffle=True, num_workers=0)
+
+        for batch, data in enumerate(loader_train, 1):
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = model(input)
+
+            show_tensor_img(label)
+            show_tensor_img(input)
+            show_tensor_img(output)
+
+            self.assertTrue((1, 3, 160, 200) == label.size())
+            self.assertTrue((1, 3, 160, 200) == input.size())
+            self.assertTrue((1, 3, 160, 200) == label.size())
 
 if __name__ == "__main__":
     unittest.main()
