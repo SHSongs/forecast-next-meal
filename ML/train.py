2db54b4c19580f9ee9d05afd109cd7799a6af3a2
SONG <simpledeveloper.songs@gmail.com>
2021-10-12 19:13:32 +0900

    train model

--- ML/train.py
+++ ML/train.py
@@ -1,6 +1,3 @@
-import torch
-import torch.nn as nn
-import matplotlib.pyplot as plt
 from torchvision import transforms
 
 from model import UNet
@@ -8,28 +5,12 @@ from dataset import *
 
 from util import show_tensor_img
 
-# model = UNet(nch=3, nker=64, norm="bnorm", learning_type="plain")
-#
-# img = plt.imread('test/test_img2.png')
-# img = torch.Tensor(img)
-# img = img.permute(2, 0, 1)
-# img = img.unsqueeze(0)
-#
-# m = nn.ZeroPad2d((0, 0, 10, 0))
-# img = m(img)
-#
-# print(img.shape)
-#
-# out = model(img)
-#
-# show_tensor_img(img)
-# show_tensor_img(out)
+lr = 1e-3
+num_epoch = 100
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 model = UNet(nch=3, nker=64, norm="bnorm", learning_type="plain")
-
 
 transform_train = transforms.Compose([Normalization(), ToTensor(), Padding()])
 
@@ -37,8 +18,32 @@ dataset_train = Dataset(label_dir="test/data/label", input_dir="test/data/input"
 
 loader_train = DataLoader(dataset_train, batch_size=1, shuffle=True, num_workers=0)
 
-for batch, data in enumerate(loader_train, 1):
-    label = data['label'].to(device)
-    input = data['input'].to(device)
+fn_loss = nn.MSELoss().to(device)
 
-    output = model(input)
+optim = torch.optim.Adam(model.parameters(), lr=lr)
+
+loss_lst = []
+
+for epoch in range(num_epoch + 1):
+    for batch, data in enumerate(loader_train, 1):
+        label = data['label'].to(device)
+        input = data['input'].to(device)
+
+        output = model(input)
+
+        optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        loss.backward()
+
+        optim.step()
+
+        loss_lst += [loss.item()]
+
+    print(epoch, loss.item())
+show_tensor_img(label)
+show_tensor_img(input)
+show_tensor_img(output)
+
+plt.plot(range(len(loss_lst)), loss_lst, color="blue")
+plt.show()
