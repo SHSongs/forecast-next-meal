2c735bac44cab4ef3bdcb7b50ad780b28ce64eb6
SONG <simpledeveloper.songs@gmail.com>
2021-10-17 14:34:30 +0900

    input img와 label 분리

--- LabellingTool/main.py
+++ LabellingTool/main.py
@@ -5,6 +5,7 @@ oldx = oldy = -1
 
 brush_size = 10
 
+
 def on_mouse(event, x, y, flags, param):
     global oldx, oldy
     global brush_size
@@ -18,17 +19,20 @@ def on_mouse(event, x, y, flags, param):
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if flags & cv2.EVENT_FLAG_LBUTTON:  # ==를 쓰면 다른 키도 입력되었을 때 작동안하므로 &(and) 사용
-            cv2.line(img, (oldx, oldy), (x, y), (0, 0, 255), brush_size, cv2.LINE_AA)
-            cv2.imshow('image', img)
+            cv2.line(label, (oldx, oldy), (x, y), (0, 0, 255), brush_size, cv2.LINE_AA)
+            dst = cv2.addWeighted(img, 0.7, label, 0.3, 0)
+            cv2.imshow('image', dst)
             oldx, oldy = x, y
 
 
-# img = np.ones((480, 640, 3), dtype=np.uint8) * 255
 img = cv2.imread("20210991.png")
 
-cv2.namedWindow('image')
+y, x, c = img.shape
+label = np.ones((y, x, c), dtype=np.uint8) * 0
 
+cv2.namedWindow('image')
 cv2.setMouseCallback('image', on_mouse, img)
+
 
 cv2.imshow('image', img)
 
