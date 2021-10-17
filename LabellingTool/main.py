3ba711f2f7c8a2ce3f1c2120df5eb2c554d6ab8e
SONG <simpledeveloper.songs@gmail.com>
2021-10-17 14:24:51 +0900

    control brush size

--- LabellingTool/main.py
+++ LabellingTool/main.py
@@ -3,9 +3,11 @@ import cv2
 
 oldx = oldy = -1
 
+brush_size = 10
 
 def on_mouse(event, x, y, flags, param):
     global oldx, oldy
+    global brush_size
 
     if event == cv2.EVENT_LBUTTONDOWN:
         oldx, oldy = x, y
@@ -16,18 +18,29 @@ def on_mouse(event, x, y, flags, param):
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if flags & cv2.EVENT_FLAG_LBUTTON:  # ==를 쓰면 다른 키도 입력되었을 때 작동안하므로 &(and) 사용
-            cv2.line(img, (oldx, oldy), (x, y), (0, 0, 255), 100, cv2.LINE_AA)
+            cv2.line(img, (oldx, oldy), (x, y), (0, 0, 255), brush_size, cv2.LINE_AA)
             cv2.imshow('image', img)
             oldx, oldy = x, y
 
 
-img = np.ones((480, 640, 3), dtype=np.uint8) * 255
+# img = np.ones((480, 640, 3), dtype=np.uint8) * 255
+img = cv2.imread("20210991.png")
 
 cv2.namedWindow('image')
 
 cv2.setMouseCallback('image', on_mouse, img)
 
 cv2.imshow('image', img)
-cv2.waitKey()
+
+while True:
+
+    key = cv2.waitKey()
+
+    if key == ord('1'):
+        brush_size -= 1
+    elif key == ord('2'):
+        brush_size += 1
+    elif key == 27:  # esc
+        break
 
 cv2.destroyAllWindows()
