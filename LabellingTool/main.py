a13df026803d975a99372e47150e21c4aab68256
SONG <simpledeveloper.songs@gmail.com>
2021-10-17 15:25:39 +0900

    change image with arrow key, window size controll

--- LabellingTool/main.py
+++ LabellingTool/main.py
@@ -1,5 +1,11 @@
 import numpy as np
 import cv2
+import os
+
+img_path = 'input'
+file_list = os.listdir(img_path)
+img_files = [file for file in file_list if file.endswith('.png')]
+img_files.sort()
 
 
 def on_mouse(event, x, y, flags, param):
@@ -26,16 +32,23 @@ oldx = oldy = -1
 
 brush_size = 10
 
-img = cv2.imread("20210991.png")
+img_index = 0
+
+
 dst = None
+
+img = cv2.imread(os.path.join(img_path, img_files[img_index]))
 
 y, x, c = img.shape
 label = np.zeros((y, x, c), dtype=np.uint8)
 
-cv2.namedWindow('image')
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)  # 창 크기 수동 조절
+cv2.moveWindow(winname='image', x=50,y=50)
+
 cv2.setMouseCallback('image', on_mouse, img)
 cv2.imshow('image', img)
 
+cv2.resizeWindow(winname='image', width=1000, height=1000)
 
 show_label = False
 
@@ -54,5 +67,24 @@ while True:
         show_label = not show_label
     elif key == 27:  # esc
         break
+
+    keyEx = cv2.waitKeyEx()
+    print(keyEx)
+    if keyEx == 63235:  # left arrow
+        img_index -= 1
+        img = cv2.imread(os.path.join(img_path, img_files[img_index]))
+        cv2.imshow('image', img)
+        print('left')
+        y, x, c = img.shape
+        label = np.zeros((y, x, c), dtype=np.uint8)
+
+    elif keyEx == 63234:  # right arrow
+        img_index += 1
+        img = cv2.imread(os.path.join(img_path, img_files[img_index]))
+        cv2.imshow('image', img)
+        print('right')
+        y, x, c = img.shape
+        label = np.zeros((y, x, c), dtype=np.uint8)
+
 
 cv2.destroyAllWindows()
