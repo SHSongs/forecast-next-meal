b3aff537b08701e63de1b2de1434856af2e05099
SONG <simpledeveloper.songs@gmail.com>
2021-10-18 17:37:05 +0900

    make load_label, setup_cv func

--- LabellingTool/main.py
+++ LabellingTool/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 import os
 
-IMG_PATH = 'input'
-LABEL_PATH = 'label'
+from util import crop_img
+from cv_util import setup_cv, load_label
 
-COLOR_LIST = [(0, 0, 255), (0, 255, 0), (0, 255, 255), (255, 0, 0), (255,  0, 255), (255, 255, 255)]
+from config import *
 
 color_idx = 0
 
@@ -49,39 +49,16 @@ img_index = 0
 
 dst = None
 
-
-def crab_img(img):
-    y, x, c = img.shape
-
-    left_x = int((x - 200) / 2)
-    up_y = int((y - 150) / 2)
-
-    img = img[up_y:up_y + 150, left_x:left_x + 200]
-
-    y, x, c = img.shape
-
-    print(x, y)
-    assert x == 200 and y == 150
-
-    return img
-
-
 img = cv2.imread(os.path.join(IMG_PATH, img_files[img_index]))
-img = crab_img(img)
+img = crop_img(img)
 
 y, x, c = img.shape
 
 label = np.zeros((y, x, c), dtype=np.uint8)
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)  # 창 크기 수동 조절
-cv2.moveWindow(winname='image', x=50, y=50)
+setup_cv(img, on_mouse=on_mouse)
 
-cv2.setMouseCallback('image', on_mouse, img)
-cv2.imshow('image', img)
 
-cv2.resizeWindow(winname='image', width=1000, height=1000)
-
-show_label = False
 
 
 def next_img(n):
@@ -95,18 +72,15 @@ def next_img(n):
     img_index += n
 
     img = cv2.imread(os.path.join(IMG_PATH, img_files[img_index]))
-    img = crab_img(img)
+    img = crop_img(img)
 
     cv2.imshow('image', img)
-    y, x, c = img.shape
 
-    if os.path.isfile(os.path.join(LABEL_PATH, img_files[img_index])):  # label file 존재시 label 불옴옴
-        label = cv2.imread(os.path.join(LABEL_PATH, img_files[img_index]))
-        dst = cv2.addWeighted(img, 0.7, label, 0.3, 0)
-        cv2.imshow('image', dst)
-    else:
-        label = np.zeros((y, x, c), dtype=np.uint8)
+    label, dst = load_label(img, img_files[img_index])
+    cv2.imshow('image', dst)
 
+
+show_label = False
 
 while True:
     key = cv2.waitKey()
